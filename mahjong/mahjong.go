355091fd1c827baf85e5c83b434e5ba4523a3197
soe-j <7115203+soe-j@users.noreply.github.com>
2020-05-06 13:54:27 +0900

    go fmt

--- mahjong/mahjong.go
+++ mahjong/mahjong.go
@@ -1,54 +1,54 @@
 package main
 
 import (
-  "fmt"
-  "math/rand"
-  "sort"
+	"fmt"
+	"math/rand"
+	"sort"
 )
 
 func main() {
-  memberNum := 24
-  members := generateMembers(memberNum)
-  fmt.Println(members)
+	memberNum := 24
+	members := generateMembers(memberNum)
+	fmt.Println(members)
 
-  shuffle(members)
-  fmt.Println(members)
+	shuffle(members)
+	fmt.Println(members)
 
-  tables := devideIntoTables(members)
-  fmt.Println(tables)
+	tables := devideIntoTables(members)
+	fmt.Println(tables)
 
-  sortByTables(tables)
-  fmt.Println(tables)
+	sortByTables(tables)
+	fmt.Println(tables)
 }
 
 func generateMembers(n int) []int {
-  arr := make([]int, n)
-  for i := range arr {
-    arr[i] = i + 1;
-  }
-  return arr
+	arr := make([]int, n)
+	for i := range arr {
+		arr[i] = i + 1
+	}
+	return arr
 }
 
 func shuffle(members []int) {
-  n := len(members)
-  for i := n - 1; i >= 0; i-- {
-    j := rand.Intn(i + 1)
-    members[i], members[j] = members[j], members[i]
-  }
+	n := len(members)
+	for i := n - 1; i >= 0; i-- {
+		j := rand.Intn(i + 1)
+		members[i], members[j] = members[j], members[i]
+	}
 }
 
 func devideIntoTables(members []int) [][]int {
-  tables := make([][]int, len(members) / 4)
-  for i := 0; i < len(members); i += 4 {
-    tables[i / 4] = members[i:i+4]
-  }
-  return tables
+	tables := make([][]int, len(members)/4)
+	for i := 0; i < len(members); i += 4 {
+		tables[i/4] = members[i : i+4]
+	}
+	return tables
 }
 
-func sortByTables (tables [][]int) {
-  for _, table := range tables {
-    sort.Slice(table, func (j ,k int) bool {
-      return table[j] < table[k];
-    })
-  }
+func sortByTables(tables [][]int) {
+	for _, table := range tables {
+		sort.Slice(table, func(j, k int) bool {
+			return table[j] < table[k]
+		})
+	}
 }
