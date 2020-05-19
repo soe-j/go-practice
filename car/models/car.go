d57362a73683bb982c32098b78070fae30447372
soe-j <7115203+soe-j@users.noreply.github.com>
2020-05-19 22:10:01 +0900

    struct hasn't to be public

--- car/models/car.go
+++ car/models/car.go
@@ -9,30 +9,30 @@ type ICar interface {
 	Stop() string
 }
 
-// Car 車構造
-type Car struct {
+// car 車構造
+type car struct {
 	name  string
 	speed int
 }
 
 // NewCar 新しい車を作る
 func NewCar(name string) ICar {
-	return &Car{name: name, speed: 0}
+	return &car{name: name, speed: 0}
 }
 
 // GetName 名前を取得する
-func (car *Car) GetName() string {
+func (car *car) GetName() string {
 	return car.name
 }
 
 // Run 走る
-func (car *Car) Run(speed int) string {
+func (car *car) Run(speed int) string {
 	car.speed = speed
 	return strconv.Itoa(speed) + "kmで走行"
 }
 
 // Stop 止まる
-func (car *Car) Stop() string {
+func (car *car) Stop() string {
 	car.speed = 0
 	return "停止"
 }
